58db096ebb470d17c8aaaa0a7424fac4890b7c2c
CAT <cavilest@gmail.com>
2023-08-31 19:05:29 -0400

    Commit inicial. Creación del repositorio.

---
+++ app/src/main/java/cl/cat2814/tdcertificacion/MainActivity.kt
@@ -0,0 +1,11 @@
+package cl.cat2814.tdcertificacion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

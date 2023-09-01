5228457a317b0f9ec051fe75dabcf2c105ee1ba6
CAT <cavilest@gmail.com>
2023-08-31 23:12:33 -0400

    Prueba de certificación Android. - Creación del paquete view donde se almacenará todo lo relacionado con las vistas de la aplicación. - Creación del archivo de colores solicitados para el diseño. - Creación del item layout para el recycler view. - Creación de un fondo decorativo. - Creación del adapter.

---
+++ app/src/main/java/cl/cat2814/tdcertificacion/view/adapter/SuperheroAdapter.kt
@@ -0,0 +1,59 @@
+package cl.cat2814.tdcertificacion.view.adapter
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import cl.cat2814.tdcertificacion.R
+import cl.cat2814.tdcertificacion.databinding.ItemLayoutSuperheroBinding
+import cl.cat2814.tdcertificacion.model.localData.SuperheroEntity
+import coil.load
+
+class SuperheroAdapter : RecyclerView.Adapter<SuperheroAdapter.ItemSuperheroViewHolder>() {
+
+    private lateinit var binding: ItemLayoutSuperheroBinding
+    private val superheroesList = mutableListOf<SuperheroEntity>()
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): SuperheroAdapter.ItemSuperheroViewHolder {
+        binding =
+            ItemLayoutSuperheroBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ItemSuperheroViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: SuperheroAdapter.ItemSuperheroViewHolder, position: Int) {
+        val superheroes = superheroesList[position]
+        holder.bind(superheroes)
+    }
+
+    override fun getItemCount(): Int {
+        return superheroesList.size
+    }
+
+    fun setData(superheroes: List<SuperheroEntity>) {
+        this.superheroesList.clear()
+        this.superheroesList.addAll(superheroes)
+        notifyDataSetChanged()
+    }
+
+    class ItemSuperheroViewHolder(val binding: ItemLayoutSuperheroBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(superhero: SuperheroEntity) {
+            binding.tvSuperheroName.text = superhero.name
+            binding.tvSuperheroBirthPlace.text = superhero.birthPlace
+            binding.ivSuperheroImage.load(superhero.imageLink)
+
+            binding.cvSuperhero.setOnClickListener {
+                val bundle = Bundle()
+                bundle.putInt("id", superhero.id)
+
+                Navigation.findNavController(binding.root)
+                    .navigate(R.id.action_superheroesListFragment_to_superheroDetailFragment)
+            }
+        }
+    }
+}
